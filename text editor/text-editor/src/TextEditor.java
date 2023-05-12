bbe09906bea25f2a25442f2994f60fd072b7660d
Nitin Sahu <sahun5774@gmail.com>
2023-05-12 08:56:37 +0530

    driver function

--- text editor/text-editor/src/TextEditor.java
+++ text editor/text-editor/src/TextEditor.java
@@ -1,6 +1,36 @@
 public class TextEditor {
     public static void main(String[] args) {
         File file = new File();
-        System.out.println("- First Project");
+        file.insert("h");
+        file.insert("e");
+        file.insert("l");
+        file.insert("l");
+        file.insert("o");
+        System.out.println(file.displayContent());
+        file.navigate("left");
+        file.navigate("left");
+        file.insert("q");
+        System.out.println(file.displayContent());
+        file.navigate("left");
+        file.backspace();
+        System.out.println(file.displayContent());
+        System.out.println("-------------------");
+        file.undo();
+        System.out.println(file.displayContent());
+        file.undo();
+        System.out.println(file.displayContent());
+        file.undo();
+        System.out.println(file.displayContent());
+        file.undo();
+        System.out.println(file.displayContent());
+        System.out.println("-------------------");
+        file.redo();
+        System.out.println(file.displayContent());
+        file.redo();
+        System.out.println(file.displayContent());
+        file.redo();
+        System.out.println(file.displayContent());
+        file.redo();
+        System.out.println(file.displayContent());
     }
 }
